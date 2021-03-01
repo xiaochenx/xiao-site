8a2054ecc8ea76aaa4c15a03891a3fb7126f405c
Xiaochen Xu <xiaochex@gmail.com>
2021-02-28 19:54:23 -0500

    commented out javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const h2 = document.createElement('h2');
-h2.textContent = "Welcome to my site!";
-document.querySelector('body').appendChild(h2);
+//const h2 = document.createElement('h2');
+//h2.textContent = "Welcome to my site!";
+//document.querySelector('body').appendChild(h2);
 
-const h3 = document.createElement ('h3');
-h3.textContent = "I really enjoy coding!";
-document.querySelector('body').appendChild(h3);
+//const h3 = document.createElement ('h3');
+//h3.textContent = "I really enjoy coding!";
+//document.querySelector('body').appendChild(h3);
